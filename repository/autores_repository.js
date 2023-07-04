74d144c464e0261255c10241bdbae1fe9d73ab41
Weslley Amaro <weslley.amaro7@gmail.com>
2023-07-04 20:46:29 -0300

    Feat: db autores completo

--- repository/autores_repository.js
+++ repository/autores_repository.js
@@ -1,16 +1,17 @@
 const liros = require('./livros_repository')
 const { Client } = require('pg')
 
-const client = new Client({
+const conection = {
     host: 'localhost',
     port: 5432,
     user: 'postgres',
     password: '123456',
-    database: 'biblioteca',
-})
+    database: 'biblioteca'
+}
 
 async function inserir(autor) {
     if (autor && autor.nome && autor.pais) {
+        const client = new Client(conection);
         let text = 'INSERT INTO autores (nome, pais) VALUES ($1, $2) RETURNING *';
         let values = [autor.nome, autor.pais];
         await client.connect();
@@ -27,6 +28,7 @@ async function inserir(autor) {
 }
 
 async function listar() {
+    const client = new Client(conection);
     await client.connect();
     const res = await client.query('SELECT * FROM autores')
     const listaAutores = res.rows;
@@ -35,21 +37,13 @@ async function listar() {
 }
 
 async function listarPorId(id) {
+    const client = new Client(conection);
     await client.connect();
-    const res = await client.query('SELECT * FROM autores')
-    const listaAutores = res.rows;
+    const res = await client.query('SELECT * FROM autores WHERE id_autor = $1', [id]);
+    const listaAutores = res.rows[0];
     await client.end();
 
-    for (let i of listaAutores) {
-        if (i.id == id) {
-            return i;
-        }
-    }
-
-    throw ({
-        numero: 404,
-        msg: "Erro: Autor não encontrado."
-    })
+    return listaAutores;
 }
 
 async function atualizar(id, autor) {
@@ -60,28 +54,28 @@ async function atualizar(id, autor) {
         })
     }
 
-    const listaAutores = listar();
-
-    for (let i = 0; i < listaAutores.length; i++) {
-        if (listaAutores[i].id == id) {
-            let text = 'UPDATE autores SET nome = ($1), pais = ($2) WHERE autores.id = ($3) RETURNING *';
-            let values = [autor.nome, autor.pais, id];
-            await client.connect();
-            const res = await client.query(text, values);
-            const autores = res.rows;
-            await client.end();
-            return autores;
-        }
+    const listaAutores = listarPorId(id);
+    
+    if (!listaAutores) {
+        throw ({
+            numero: 404,
+            msg: "Erro: Autor não encontrado."
+        })
     }
+    
+    const client = new Client(conection);
+    let text = 'UPDATE autores SET nome = $1, pais = $2 WHERE id_autor = $3 RETURNING *';
+    let values = [autor.nome, autor.pais, id];
+    await client.connect();
+    const res = await client.query(text, values);
+    const autores = res.rows;
+    await client.end();
+    return autores;
 
-    throw ({
-        numero: 404,
-        msg: "Erro: Autor não encontrado."
-    })
 }
 
 async function deletar(id) {
-    const listaLivros = liros.listar()
+    const listaLivros = livros.listar()
     for (let i = 0; i < listaLivros.length; i++) {
         if (listaLivros.autor == id) {
             throw ({
@@ -91,22 +85,23 @@ async function deletar(id) {
         }
     }
 
-    const listaAutores = listar();
-    for (let i = 0; i < listaAutores.length; i++) {
-        if (listaAutores[i].id == id) {
-            let text = "DELETE FROM autores WHERE autores.id = ($1) RETURNING *";
-            let values = [id];
-            await client.connect();
-            const res = await client.query(text, values);
-            const autores = res.rows;
-            await client.end();
-            return autores;
-        }
+    const listaAutores = listarPorId(id);
+    if (!listaAutores) {
+        throw ({
+            numero: 404,
+            msg: "Erro: Autor não encontrado."
+        })
     }
-    throw ({
-        numero: 404,
-        msg: "Erro: Autor não encontrado."
-    })
+    
+    const client = new Client(conection);
+    let text = "DELETE FROM autores WHERE id_autor = $1 RETURNING *";
+    let values = [id];
+    await client.connect();
+    const res = await client.query(text, values);
+    const autores = res.rows;
+    await client.end();
+    return autores;
+    
 }
 
 module.exports = {
